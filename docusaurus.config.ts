a774fc2d702511422e760fe710f190e290929a71
Giovanni Zarrillo <gazarrillo1105@gmail.com>
2026-03-09 12:59:24 -0400

    fix: update production URL for documentation site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://quoota-technologies.github.io',
+  url: 'https://docs.quoota.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
